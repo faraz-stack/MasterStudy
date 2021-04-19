147187bfbd9728428e11f532916b973ed9da7b37
king-faraz <faraznaeem@gap-dynamics.com>
2021-04-20 01:22:31 +0500

    hello world

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  View,
+  Text
+}from 'react-native';
+
+function App(props){
+  return(
+    <View>
+      <Text>hello world</Text>
+    </View>
+  )
+} 
+export default App;
